8f4062cbc85b61fe3c11e2f9db97388e9e3b3ceb
udaye <udayeswari.savana@gmail.com>
2023-04-24 16:08:23 -0400

    groovy files

---
+++ vars/nodejs.groovy
@@ -0,0 +1,51 @@
+pipeline {
+
+    agent any
+
+    stages {
+
+        stage('code quality') {
+            steps {
+                echo 'code quality'
+                sh 'env'
+            }
+        }
+        stage('style checks') {
+            when {
+                branch 'master'
+            }
+            steps {
+                echo 'code quality'
+            }
+        }
+
+        stage('unit tests') {
+            when {
+                branch 'master'
+            }
+            steps {
+                echo 'unit test'
+            }
+        }
+
+
+        stage('download dependencies') {
+            when { tag "*" }
+            steps {
+                echo 'download dependencies'
+            }
+        }
+
+        stage('prepare artifact') {
+            when { tag "*" }
+            steps {
+                echo 'prepare artifacts'
+            }
+        }
+        stage('publish artifacts') {
+            steps {
+                echo 'publish artifact'
+            }
+        }
+    }
+}
